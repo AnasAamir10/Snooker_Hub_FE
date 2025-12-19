6d7277b8c67420fd307b2f997c06122bc37e87d2
AnasAamir10 <93116546+AnasAamir10@users.noreply.github.com>
2025-12-19 11:22:54 +0500

    Wrap App component in ErrorBoundary

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { ErrorBoundary } from 'react-error-boundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>,
 )
